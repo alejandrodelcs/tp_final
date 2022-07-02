925c3dfd90555aaac9b4b9785fb8609eea4a3734
Alejandro <alejodc15@gmail.com>
2022-07-02 19:43:29 -0300

    cambios

--- AuthorsFileParser.h
+++ AuthorsFileParser.h
@@ -29,27 +29,27 @@ private:
 
     /*
      * PRE:
-     * POST: retorna el ID (numero de referencia) de la linea del fichero
+     * POST: returns the ID (reference number) of the line of the file
      */
     int getISNI();
     /*
      * PRE:
-     * POST: retorna el nombre de la linea del fichero
+     * POST: returns the filename of the line in the file
      */
     bool validateFileLine();
     /*
      * PRE:
-     * POST: Inicializa los datos traidos del archivos escritores
+     * POST: Initializes the data brought in from the writers' files
      */
     void validateAuthorFile(int count);
     /*
      * PRE:
-     * POST: Retorna el número al que se hará referencia en el archivo de lecturas
+     * POST: Returns the number to be referenced in the readings file.
      */
     bool newReference();
     /*
      * PRE:
-     * POST: Valida el ultimo escritor del archivo
+     * POST: Validates the last writer of the file
      */
     void validateEOFAuthor(int count);
 
@@ -69,27 +69,27 @@ private:
 public:
     /* Constructor
      * PRE:
-     * POST: construye un nuevo parserEscritor
+     * POST:
      */
     AuthorsFileParser();
     /*
      * PRE:
-     * POST: Retorna la tabla de hash con los autores
+     * POST: Returns the hash table with the authors
      */
     HashTable<Author *>* getAuthors();
     /*
      * PRE:
-     * POST: Agrega el autor a su lectura
+     * POST: Adds the author to his reading
      */
     void addReadingAuthor();
     /*
      * PRE:
-     * POST: Setea la lista lecturas en Readings File Parser
+     * POST: Set the list of readings to  Readings File Parser
      */
     void setReadings(List<Reading*>* &l);
     /*
      * PRE:
-     * POST: Setea la tabla de hash  en Readings File Parser
+     * POST: Set the table hash of Readings File Parser
      */
     void setAuthors(HashTable<Author*>* &a);
     /*
@@ -99,7 +99,7 @@ public:
     void displayAuthors();
     /*
      * PRE:
-     * POST:
+     * POST:  author's data request
      */
     void requestAuthorsInfo();
     /*
