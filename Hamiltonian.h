df3fe286d0cb4c37229cd2bbe435846fbba3d625
IA104 <iananca104@gmail.com>
2022-07-03 00:09:43 -0300

    Arreglos de traduccion

--- Hamiltonian.h
+++ Hamiltonian.h
@@ -16,98 +16,89 @@ private:
     Validation validation;
     bool explanation;
     List<List<Reading*>*>* minimalOrders;
-
     /*
      * PRE:
      * POST:
      */
     void displayPossibileOrder(int option);
-
     /*
      * PRE:
      * POST:
      */
     void ordersMenu(int totalTime);
-
     /*
      * PRE:
      * POST:
      */
     void explainProcess();
-
     /*
      * PRE:
      * POST:
      */
     void cloneReadingsArray(Reading *A[], Reading *B[]);
-
     /*
      * PRE:
      * POST:
      */
     void hamiltonianRecursion(Reading *minimalOrder[], int currentID, Reading *currentOrder[], bool visited[], int arraySize, int acumulatedTime);
-
     /*
      * PRE:
      * POST:
      */
     void calculateHamiltonianShortestReadingTime(Reading *minimalOrder[]);
-
     /*
      * PRE:
      * POST:
      */
     void addArrayToList(Reading *B[]);
-
     /*
      * PRE:
      * POST:
      */
     void
     hamiltonianRecursion(int currentID, Reading *currentOrder[], bool visited[], int arraySize, int acumulatedTime);
-
     /*
      * PRE:
      * POST:
      */
     void newRecord(Reading *minimalOrder[], Reading *currentOrder[], int acumulatedTime);
-
     /*
      * PRE:
      * POST:
      */
     void newRecord(Reading *currentOrder[], int acumulatedTime);
-
     /*
      * PRE:
      * POST:
      */
     void displayOrder(Reading *currentOrder[], int acumulatedTime);
-
     /*
      * PRE:
      * POST:
      */
-
     int getLinkCost(int currentID, Reading *currentOrder[], int arraySize, int i);
-
+    /*
+     * PRE:
+     * POST:
+     */
     void calculateShortestReadingTimes();
-
 public:
+    /* Constructor
+     * PRE:
+     * POST:
+     */
     Hamiltonian();
     /*
      * PRE:
      * POST:
      */
     void getShortestReadingsTime(List<Reading *> *readings, ReadingsFileParser *pReadings);
-
     /*
      * PRE:
      * POST:
      */
     void getShortestReadingsTimes(List<Reading *> *readings, ReadingsFileParser *pReadings);
-
-    /*
+    /* Destructor
      * PRE:
      * POST
      */
